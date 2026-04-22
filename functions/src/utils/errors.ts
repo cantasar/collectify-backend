f4c2b3b550c91904463971d2e0a24791edb22a9c
cantasar <62062865+cantasar@users.noreply.github.com>
2026-04-22 17:33:45 +0300

    feat: add error classes

---
+++ functions/src/utils/errors.ts
@@ -0,0 +1,29 @@
+export class AppError extends Error {
+  public readonly statusCode: number;
+  public readonly code: string;
+
+  constructor(statusCode: number, code: string, message: string) {
+    super(message);
+    this.name = this.constructor.name;
+    this.statusCode = statusCode;
+    this.code = code;
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message = "Authentication required") {
+    super(401, "UNAUTHORIZED", message);
+  }
+}
+
+export class ForbiddenError extends AppError {
+  constructor(message = "Access denied") {
+    super(403, "FORBIDDEN", message);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message = "Resource not found") {
+    super(404, "NOT_FOUND", message);
+  }
+}
